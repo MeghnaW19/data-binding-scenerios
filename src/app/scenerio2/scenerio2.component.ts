ea1bae2fd2448c3449c03ef34424bf400f0ca394
Meghna <meghna@stackroute.in>
2019-05-31 16:26:28 +0530

    scenerio2

--- src/app/scenerio2/scenerio2.component.ts
+++ src/app/scenerio2/scenerio2.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scenerio2.component.css']
 })
 export class Scenerio2Component implements OnInit {
-
+  title='Single Component Two way binding via NgModel';
   constructor() { }
 
   ngOnInit() {

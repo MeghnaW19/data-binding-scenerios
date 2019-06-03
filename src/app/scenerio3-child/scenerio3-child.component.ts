0a5746f039e65b15fbd586662b0aa23bb0da9196
Meghna <meghna@stackroute.in>
2019-06-03 11:59:50 +0530

    created scenerio3 components

---
+++ src/app/scenerio3-child/scenerio3-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scenerio3-child',
+  templateUrl: './scenerio3-child.component.html',
+  styleUrls: ['./scenerio3-child.component.css']
+})
+export class Scenerio3ChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/scenerio3-parent/scenerio3-parent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scenerio3-parent',
+  templateUrl: './scenerio3-parent.component.html',
+  styleUrls: ['./scenerio3-parent.component.css']
+})
+export class Scenerio3ParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

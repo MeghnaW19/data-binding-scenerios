c92580783c731d9126ef6248454586e047b37b02
Meghna <meghna@stackroute.in>
2019-06-04 15:33:50 +0530

    scenerio4

--- src/app/scenerio4-child/scenerio4-child.component.ts
+++ src/app/scenerio4-child/scenerio4-child.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-scenerio4-child',
-  templateUrl: './scenerio4-child.component.html',
-  styleUrls: ['./scenerio4-child.component.css']
+  selector: "app-scenerio4-child",
+  templateUrl: "./scenerio4-child.component.html",
+  styleUrls: ["./scenerio4-child.component.css"]
 })
 export class Scenerio4ChildComponent implements OnInit {
+  title: string = "Passing Data from Child to Parent";
+  message: string = "Message from Child";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  @Output() messageEvent = new EventEmitter<any>();
+
+  sendMessage() {
+    // console.log("**", this.message);
+    this.messageEvent.emit(this.message);
   }
-
 }
